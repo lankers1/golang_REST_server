52b7ef62f06b1adee9a67dc34879d5dae4237f95
jciaramella1 <james.ciaramella@visformatics.net>
2021-11-14 14:11:48 +0000

    feat: pass PlayerStore to PlayerServer instance

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"net/http"
 )
 
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) string {
+	return "123"
+}
+
 //The HandlerFunc type is an adapter to allow the use of ordinary functions as HTTP handlers.
 //ListenAndServe listens on TCP network on the provided port and calls the handler to handle requests on incoming connections
 func main() {
-	server := &PlayerServer{}
+	server := &PlayerServer{&InMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
