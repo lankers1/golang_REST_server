a2bddccec0d4ebd5cf9ac14d0b4782bf4c357416
jciaramella1 <james.ciaramella@visformatics.net>
2021-11-14 14:13:02 +0000

    chore: remove unused function

--- server.go
+++ server.go
@@ -22,15 +22,3 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
-
-func GetPlayerScore(name string) string {
-	if name == "Pepper" {
-		return "20"
-	}
-
-	if name == "Floyd" {
-		return "10"
-	}
-
-	return ""
-}
