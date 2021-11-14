441bb771b390c304dd3a847a600c0f49211aba6a
jciaramella1 <james.ciaramella@visformatics.net>
2021-11-14 13:49:56 +0000

    fix: handle player name path param

--- server.go
+++ server.go
@@ -3,10 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 //ResponseWriter implements the IO Writer with the Write function.
 //Request represents an HTTP request received by a server or to be sent by a client.
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+
+	//TrimPrefix removes "/players/" from URL path string to return the players name
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
 }
