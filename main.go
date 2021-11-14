5f037e6c1656383b319f00b3441c539b7344d5b1
jciaramella1 <james.ciaramella@visformatics.net>
2021-11-14 13:45:58 +0000

    feat: setup server and handle connections to port 5000

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
+//The HandlerFunc type is an adapter to allow the use of ordinary functions as HTTP handlers.
+//ListenAndServe listens on TCP network on the provided port and calls the handler to handle requests on incoming connections
 func main() {
-	return
+	handler := http.HandlerFunc(PlayerServer)
+	log.Fatal(http.ListenAndServe(":5000", handler))
 }
