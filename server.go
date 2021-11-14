228dfc0175bcd45b154928b62fbf061c0b5fe7d9
jciaramella1 <james.ciaramella@visformatics.net>
2021-11-14 18:21:26 +0000

    refactor: extract method handlers to separate functions

--- server.go
+++ server.go
@@ -18,11 +18,16 @@ type PlayerServer struct {
 //Request represents an HTTP request received by a server or to be sent by a client.
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return
+	//makes routing clearer
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
 	}
+}
 
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	//TrimPrefix removes "/players/" from URL path string to return the players name
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	score := p.store.GetPlayerScore(player)
@@ -32,4 +37,8 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
 }
