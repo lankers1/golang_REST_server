2cb02fe9b166bb17d5db473defac5ff55a97e1a6
jciaramella1 <james.ciaramella@visformatics.net>
2021-11-14 13:38:24 +0000

    feat: initial PlayerServer implementation

--- server.go
+++ server.go
@@ -1,5 +1,12 @@
 package main
 
-func server() {
-	return
+import (
+	"fmt"
+	"net/http"
+)
+
+//ResponseWriter implements the IO Writer with the Write function.
+//Request represents an HTTP request received by a server or to be sent by a client.
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "20")
 }
