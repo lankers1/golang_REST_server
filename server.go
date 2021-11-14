9ec498892e92396ccf74a086f9096882a4a750bf
jciaramella1 <james.ciaramella@visformatics.net>
2021-11-14 18:17:34 +0000

    feat: pass test

--- server.go
+++ server.go
@@ -17,8 +17,13 @@ type PlayerServer struct {
 //ResponseWriter implements the IO Writer with the Write function.
 //Request represents an HTTP request received by a server or to be sent by a client.
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	//TrimPrefix removes "/players/" from URL path string to return the players name
 
+	if r.Method == http.MethodPost {
+		w.WriteHeader(http.StatusAccepted)
+		return
+	}
+
+	//TrimPrefix removes "/players/" from URL path string to return the players name
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	score := p.store.GetPlayerScore(player)
 
