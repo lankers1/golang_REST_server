89d48e5328b71af77b0dc783c7e0bedf1cee5885
jciaramella1 <james.ciaramella@visformatics.net>
2021-11-15 16:12:45 +0000

    feat: RecordWin receiver function

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func (i *InMemoryPlayerStore) GetPlayerScore(name string) string {
 	return "123"
 }
 
+func (i *InMemoryPlayerStore) RecordWin(name string) {}
+
 //The HandlerFunc type is an adapter to allow the use of ordinary functions as HTTP handlers.
 //ListenAndServe listens on TCP network on the provided port and calls the handler to handle requests on incoming connections
 func main() {
