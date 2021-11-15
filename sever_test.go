3d9d3f6848ed31e659ced9108783723cc321f91d
jciaramella1 <james.ciaramella@visformatics.net>
2021-11-15 16:10:39 +0000

    test: POST to /players/:player records a win

--- sever_test.go
+++ sever_test.go
@@ -27,6 +27,7 @@ func TestGETPlayers(t *testing.T) {
 			"Pepper": "20",
 			"Floyd":  "10",
 		},
+		nil,
 	}
 	server := &PlayerServer{&store}
 
@@ -63,25 +64,27 @@ func TestGETPlayers(t *testing.T) {
 func TestStoreWins(t *testing.T) {
 	store := StubPlayerStore{
 		map[string]string{},
+		nil,
 	}
 	server := &PlayerServer{&store}
-	t.Run("it returns accepted on POST", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
+	t.Run("it records wins on POST", func(t *testing.T) {
+		player := "Pepper"
+
+		request := newPostWinRequest(player)
 		response := httptest.NewRecorder()
+
 		server.ServeHTTP(response, request)
 
 		assertStatus(t, response.Code, http.StatusAccepted)
-	})
 
-	t.Run("it records wins when POST", func(t *testing.T) {
-		request := newPostWinRequest("Pepper")
-		response := httptest.NewRecorder()
-		server.ServeHTTP(response, request)
-
-		assertStatus(t, response.Code, http.StatusAccepted)
 		if len(store.winCalls) != 1 {
 			t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
 		}
+
+		if store.winCalls[0] != player {
+			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
+		}
+
 	})
 }
 
