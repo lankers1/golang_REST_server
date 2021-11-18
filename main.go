c221a4c0e50707f22a07c8619d1533c3fbe09eec
jciaramella1 <james.ciaramella@visformatics.net>
2021-11-18 18:46:35 +0000

    feat: POST method handler and mux setup

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -15,28 +17,41 @@ import (
 type Person struct {
 	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name  string             `json:"name,omitempty" bson:"name,omitempty"`
-	Score int16              `json:"score,omitempty" bson:"score,omitempty"`
+	Score int                `json:"score,omitempty" bson:"score,omitempty"`
 }
 
 var client *mongo.Client
 
-//The HandlerFunc type is an adapter to allow the use of ordinary functions as HTTP handlers.
-//ListenAndServe listens on TCP network on the provided port and calls the handler to handle requests on incoming connections
+func CreatePersonEndpoint(response http.ResponseWriter, request *http.Request) {
+	fmt.Print("POST started")
+	response.Header().Add("content-type", "application/json")
+	person := Person{}
+	_ = json.NewDecoder(request.Body).Decode(&person)
+
+	collection := client.Database("testdatabase").Collection("people")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, err := collection.InsertOne(ctx, person)
+	fmt.Println(result, "result")
+
+	if err != nil {
+		panic("Error")
+	}
+
+	json.NewEncoder(response).Encode(result)
+}
+
 func main() {
 	fmt.Println("Starting application...")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	clientOpts := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(ctx, clientOpts)
+	clientOpts := options.Client().ApplyURI("mongodb://mongodb")
+	client, _ = mongo.Connect(ctx, clientOpts)
 
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			panic(err)
-		}
-	}()
-
-	server := &PlayerServer{NewInMemoryPlayerStore()}
-	log.Fatal(http.ListenAndServe(":5000", server))
+	router := mux.NewRouter()
+	router.HandleFunc("/person", CreatePersonEndpoint).Methods("POST")
+	fmt.Println("server listening on port: 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
