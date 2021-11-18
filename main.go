eb2bab020ff5f9e967f4c9bad20a12a4a3760de6
jciaramella1 <james.ciaramella@visformatics.net>
2021-11-18 16:37:16 +0000

    feat: initial mongodb connection

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Person struct {
@@ -19,6 +23,20 @@ var client *mongo.Client
 //The HandlerFunc type is an adapter to allow the use of ordinary functions as HTTP handlers.
 //ListenAndServe listens on TCP network on the provided port and calls the handler to handle requests on incoming connections
 func main() {
+	fmt.Println("Starting application...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOpts := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(ctx, clientOpts)
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
 	server := &PlayerServer{NewInMemoryPlayerStore()}
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
